7ef08e7b280363abf3a8c06d0610c0286aec4e67
Gisembert <isembert.guillaume@hotmail.fr>
2019-02-21 16:51:02 +0100

    add chainecode directory

---
+++ chainecode/village/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Village = require('./lib/village');
+
+module.exports.Village = Village;
+module.exports.contracts = [ Village ];
